1343f96287f046befce9e9d35f3e6cd347f5527a
Somphoph <somphop.homprasert@krungthai.com>
2022-01-10 13:44:32 +0700

    Add page for call service.

--- src/main/java/com/example/demowebflux/controller/CustomerController.java
+++ src/main/java/com/example/demowebflux/controller/CustomerController.java
@@ -3,17 +3,15 @@ package com.example.demowebflux.controller;
 import com.example.demowebflux.domain.Customer;
 import com.example.demowebflux.repository.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
-
 @Slf4j
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 @RestController
 public class CustomerController {
 
@@ -26,7 +24,11 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     private Mono<Customer> getCustomerById(@PathVariable int id) {
-        log.info("get by id > {}", id);
         return customerRepository.findById(id);
+    }
+
+    @GetMapping
+    private Flux<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 }
